df49dd8cda15b390f1189f6687a9e4ce68ff6c6a
sidh0050 <sattisidhu.kaur7@gmail.com>
2012-06-11 13:07:16 -0400

    This is filters related dinosaurs file in which i add some filters in my add.php file.

--- dinosaurs/add.php
+++ dinosaurs/add.php
@@ -1,4 +1,25 @@
-<!DOCTYPE HTML>
+<?php
+$errors=array();
+
+$dino_name=filter_input(INPUT_POST,'dino_name',FILTER_SANITIZE_STRING);
+$loves_meat=filter_input(INPUT_POST,'loves_meat',FILTER_SANITIZE_NUMBER);
+$in_jurassic_park=(isset($_POST['in_jurassic_park']))?1:0;
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+	if (strlen($dino_name) <0 ||strlen($dino_name) >256 ){
+		$errors['dino_name']=true;
+	}
+	
+	if(!in_array($loves_meat,array(0,1))){
+		$errors['loves_meat']=true;
+	}
+	
+	if(empty($errors)){
+		//Do Db stuff
+	}
+}
+
+?><!DOCTYPE HTML>
 <html>
 <head>
 <meta charset="utf-8">
@@ -12,22 +33,37 @@
 <form method="post" action="add.php">
 
 <div>
+<label for="dino_name">
+Dinosaur Name
+<?php if(isset($errors['dino_name'])):?>
+<strong class="errors">is required</strong>
+<?php endif; ?>
+</label>
 
 <label for="dino_name">Dinosaur Name</label>
 <input id="dino_nmae" name="dino_name" required>
 </div>
 
 <fieldset>
-<legend>Relationship With Meat</legend>
+<legend>
+Relationship With Meat
+<?php if(isset($errors['loves_meat'])):?>
+<strong class="error">is required</strong>
+<?php endif; ?>
+
+</legend>
 <input type="radio" id="love" name="loves_meat" value="1">
+<?php if($loves_meat==1): ?>checked<?php endif;?>>
 <label for="love">Loves Meat</label>
 
 <input type="radio" id="hate" name="loves_meat" value="0">
+<?php if($loves_meat==0): ?>checked<?php endif;?>>
 <label for="hate">Hates Meat</label>
 </fieldset>
 
 <div>
 <input type="checkbox" id="in_jurassic_park" name="in_jurassic_park">
+<?php if($in_jurassic_park==1): ?>checked<?php endif;?>>
 <label for="in_jurassic_park">In Jurassic Park?</label>
 </div>
 
