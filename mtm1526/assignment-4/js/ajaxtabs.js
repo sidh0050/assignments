dd6bfcf4bcb323dcaae30895d2eb72340bfd060f
sidh0050 <sattisidhu.kaur7@gmail.com>
2012-06-21 11:08:06 -0400

    My java script file for ajax tabs.

---
+++ mtm1526/assignment-4/js/ajaxtabs.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+	
+	$.get('includes/earth.html',function(data){
+		$('#gethtml').prepend(data);
+	});
+	
+	
+
+	
+	$('#mars').on('click',function(e){
+			e.preventDefault();
+		$('#gethtml').load('includes/mars.html');
+		
+	
+		
+	});
+	
+
+	
+	$('#saturn').on('click',function(e){
+		e.preventDefault();
+		$('#gethtml').load('includes/saturn.html');
+		
+	
+		
+	});
+	
+	$('#venus').on('click',function(e){
+		e.preventDefault();
+		$('#gethtml').load('includes/venus.html');
+		
+	
+		
+	});
+	
+	$('#pluto').on('click',function(e){
+		e.preventDefault();
+		$('#gethtml').load('includes/pluto.html');
+		
+	
+		
+	});
+	
+	$('#sun').on('click',function(e){
+		e.preventDefault();
+		$('#gethtml').load('includes/sun.html');
+		
+	
+		
+	});
+
+	});
